4d9049172bc5a96124baa5eae19822fc3ec4afad
Dissdiw <adilitc.05@gmail.com>
2024-01-08 13:37:17 +0700

    change register logo and form

--- coop/resources/views/auth/register.blade.php
+++ coop/resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form class="row g-3" method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Image -->
@@ -9,43 +9,43 @@
             <x-input-error :messages="$errors->get('image')" class="mt-2" />
         </div>
 
-        <!-- Userid -->
-        <div class="mt-4">
-            <x-input-label for="studentid" :value="__('Studentid')" />
-            <x-text-input id="studentid" class="block mt-1 w-full" type="text" name="studentid" :value="old('studentid')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('studentid')" class="mt-2" />
-        </div>
-
         <!-- Firstname -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="firstname" :value="__('Firstname')" />
             <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
             <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
         </div>
 
         <!-- Lastname -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="lastname" :value="__('Lastname')" />
             <x-text-input id="lastname" class="block mt-1 w-full" type="text" name="lastname" :value="old('lastname')" required autofocus autocomplete="lastname" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Phoneno -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="phoneno" :value="__('Phone number')" />
             <x-text-input id="phoneno" class="block mt-1 w-full" type="text" name="phoneno" :value="old('phoneno')" required autocomplete="phoneno" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+         <!-- Userid -->
+         <div class="mt-4">
+            <x-input-label for="studentid" :value="__('Studentid')" />
+            <x-text-input id="studentid" class="block mt-1 w-full" type="text" name="studentid" :value="old('studentid')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('studentid')" class="mt-2" />
+        </div>
+
         <!-- Password -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
@@ -57,7 +57,7 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
+        <div class="col-6 mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
