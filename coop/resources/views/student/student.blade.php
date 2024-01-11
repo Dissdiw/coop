d995b09f36d079e0f3ad02b5f092c0069617b7a9
Dissdiw <adilitc.05@gmail.com>
2024-01-11 15:54:23 +0700

    profile edit (Not yet)

--- coop/resources/views/student/student.blade.php
+++ coop/resources/views/student/student.blade.php
@@ -69,7 +69,7 @@
                 <h5 class="fw-bold mb-0 me-auto">ขั้นตอนสหกิจศึกษา</h5>
                 <div class="dropdown">
                     <div class="d-flex align-items-center cursor-pointer dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="me-2 d-none d-sm-block"></span>
+                        <span class="me-2 d-none d-sm-block">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</span>
                         <img class="navbar-profile-image" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="Image">
                     </div>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -91,40 +91,49 @@
                         <img class="edit-profile" src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image">
                         </div>
                         <div class="d-flex justify-content-center">
-                        <!-- <button class="btn btn-primary mb-2">Browse</button> -->
-                        <input type="file" id="file" name="file" accept="application/zip,application/x-rar-compressed" required>
+                        <input type="file" id="file" name="file" accept="" >
                         </div>
-                        <form>
-                        <div class="duo">
-                            <div class="me-4 mb-3">
-                            <label for="firstname" class="col-form-label">ชื่อ:</label>
-                            <input type="text" class="form-control" id="firstname" value="">
+                        <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+                            @csrf
+                        </form>
+
+                        <form method="post" action="{{ route('profile.update') }}">
+                            @csrf
+                            @method('patch')
+                            <div class="duo">
+                                <div class="me-4 mb-3">
+                                    <label for="firstname" class="col-form-label">ชื่อ:</label>
+                                    <input type="text" class="form-control" id="firstname" name="firstname" :value="old('firstname', $user->firstname)" required autofocus autocomplete="firstname">
+                                    <x-input-error class="mt-2" :messages="$errors->get('firstname')" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="lastname" class="col-form-label">นามสกุล:</label>
+                                    <input type="text" class="form-control" id="lastname" name="lastname" :value="old('lastname', $user->lastname)" required autofocus autocomplete="lastname">
+                                    <x-input-error class="mt-2" :messages="$errors->get('lastname')" />
+                                </div>
                             </div>
-                            <div class="mb-3">
-                            <label for="lastname" class="col-form-label">นามสกุล:</label>
-                            <input type="text" class="form-control" id="lastname">
+                            <div class="duo">
+                                <div class="me-4 mb-3">
+                                    <label for="personnelid" class="col-form-label">รหัสประจำตัว:</label>
+                                    <input type="text" class="form-control" id="userid" name="userid" :value="old('userid', $user->userid)" required autofocus autocomplete="userid">
+                                    <x-input-error class="mt-2" :messages="$errors->get('userid')" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="col-form-label">Password:</label>
+                                    <input type="text" class="form-control" id="password">
+                                </div>
                             </div>
-                        </div>
-                        <div class="duo">
-                            <div class="me-4 mb-3">
-                            <label for="personnelid" class="col-form-label">รหัสประจำตัว:</label>
-                            <input type="text" class="form-control" id="personnelid" disabled>
+                            <div class="duo">
+                                <div class="me-4 mb-3">
+                                    <label for="email" class="col-form-label">Email:</label>
+                                    <input type="text" class="form-control" id="email" name="email" :value="old('email', $user->email)" required autofocus autocomplete="email">
+                                    <x-input-error class="mt-2" :messages="$errors->get('email')" />
+                                </div>
+                                <div class="mb-3">
+                                    <label for="phoneno" class="col-form-label">เบอร์โทรศัพท์:</label>
+                                    <input type="text" class="form-control" id="phoneno" name="phoneno" :value="old('phoneno', $user->phoneno)" required autofocus autocomplete="phoneno">
+                                </div>
                             </div>
-                            <div class="mb-3">
-                            <label for="password" class="col-form-label">Password:</label>
-                            <input type="text" class="form-control" id="password">
-                            </div>
-                        </div>
-                        <div class="duo">
-                            <div class="me-4 mb-3">
-                            <label for="email" class="col-form-label">Email:</label>
-                            <input type="text" class="form-control" id="email">
-                            </div>
-                            <div class="mb-3">
-                            <label for="phoneno" class="col-form-label">เบอร์โทรศัพท์:</label>
-                            <input type="text" class="form-control" id="phoneno">
-                            </div>
-                        </div>
                         </form>
                     </div>
                     <div class="modal-footer">
